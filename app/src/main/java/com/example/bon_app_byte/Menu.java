191e8f58493d2aebc5f480c997b53ec36290da1e
vidhathrig <vidhathri98@gmail.com>
2019-05-30 10:02:02 +0530

    -update

--- app/src/main/java/com/example/bon_app_byte/Menu.java
+++ app/src/main/java/com/example/bon_app_byte/Menu.java
@@ -6,9 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.TextView;
+
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,7 +23,7 @@ public class Menu extends AppCompatActivity {
         FirebaseDatabase menuDatabase;
         DatabaseReference myRef;
         List <Dish> menu ;
-        TableLayout table = (TableLayout)findViewById(R.id.TableLayout);
+     //   TableLayout table = (TableLayout)findViewById(R.id.TableLayout);
         public class Dish {
 
             private String Name;
@@ -56,7 +54,7 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        final Context context = getApplicationContext();
+       // final Context context = getApplicationContext();
 
         menuDatabase = FirebaseDatabase.getInstance();
         myRef = menuDatabase.getReference().child("dish");
@@ -70,7 +68,7 @@ public class Menu extends AppCompatActivity {
                     String price =ds.getValue(String.class);
                     Dish d = new Dish(name,price);
                     menu.add(d);
-                 /*   TableRow row= new TableRow(context);
+                 /* TableRow row= new TableRow(context);
                     TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
                     row.setLayoutParams(lp);
                     TextView idx = new TextView(context);
